2f5bd8141a1146a362d3b09862fbcb6de9e77f5c
D-Kale <a3235633@gmail.com>
2024-12-30 15:22:18 -0600

    cors added

--- reconocimiento_facial/settings.py
+++ reconocimiento_facial/settings.py
@@ -38,12 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'corsheaders',
     'reconocimiento'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+
+    'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -123,3 +127,17 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://tudominio.com",
+]
+
+# Permitir el envío de credenciales (cookies, CSRF)
+CORS_ALLOW_CREDENTIALS = True
+
+CSRF_TRUSTED_ORIGINS = [
+    'http://localhost:8000',  # Asegúrate de agregar la URL de tu backend
+    'http://localhost:3000',  # Si usas frontend en localhost:3000
+]
